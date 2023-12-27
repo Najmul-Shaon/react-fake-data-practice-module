8ee8761d300b7acff9a8db50422ab32bc0c6d016
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-12-27 23:35:45 +0600

    fake data load from js object

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Users from "./Users";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const users = [
+    {
+      id: 1,
+      username: "john_doe",
+      email: "john@example.com",
+      age: 30,
+    },
+    {
+      id: 2,
+      username: "jane_smith",
+      email: "jane@example.com",
+      age: 25,
+    },
+    {
+      id: 3,
+      username: "sam_jones",
+      email: "sam@example.com",
+      age: 28,
+    },
+    {
+      id: 4,
+      username: "emily_wilson",
+      email: "emily@example.com",
+      age: 32,
+    },
+    {
+      id: 5,
+      username: "alex_brown",
+      email: "alex@example.com",
+      age: 27,
+    },
+    {
+      id: 6,
+      username: "lisa_jackson",
+      email: "lisa@example.com",
+      age: 29,
+    },
+    {
+      id: 7,
+      username: "michael_thomas",
+      email: "michael@example.com",
+      age: 35,
+    },
+    {
+      id: 8,
+      username: "sarah_anderson",
+      email: "sarah@example.com",
+      age: 26,
+    },
+    {
+      id: 9,
+      username: "william_clark",
+      email: "william@example.com",
+      age: 31,
+    },
+    {
+      id: 10,
+      username: "olivia_wright",
+      email: "olivia@example.com",
+      age: 24,
+    },
+    {
+      id: 11,
+      username: "daniel_miller",
+      email: "daniel@example.com",
+      age: 33,
+    },
+    {
+      id: 12,
+      username: "chloe_taylor",
+      email: "chloe@example.com",
+      age: 22,
+    },
+    {
+      id: 13,
+      username: "ethan_hall",
+      email: "ethan@example.com",
+      age: 28,
+    },
+    {
+      id: 14,
+      username: "ava_johnson",
+      email: "ava@example.com",
+      age: 30,
+    },
+    {
+      id: 15,
+      username: "noah_anderson",
+      email: "noah@example.com",
+      age: 29,
+    },
+  ];
+
+  // console.log(users[0].id);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fake Data</h1>
+      {users.map((user) => (
+        <Users user={user}></Users>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Users.jsx
@@ -0,0 +1,15 @@
+export default function Users({ user }) {
+  const { username, age, id } = user;
+  console.log(user);
+  return (
+    <div>
+      <small>ID: {id}</small>
+      <h3>
+        Name: <u>{username}</u>
+      </h3>
+      <p>Age: {age}</p>
+      <hr />
+      <br />
+    </div>
+  );
+}
